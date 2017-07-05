7e5551db9586cbf92fceeead55245d0bff1ccd73
Luc Suryo <luc@suryo.com>
2017-07-04 21:35:53 -0700

    costmetics

--- zookeeper/zookeper.go
+++ zookeeper/zookeper.go
@@ -3,14 +3,14 @@
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-//   * Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-//   * Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the distribution.
-//   * Neither the name of the <organization> nor the
-//   names of its contributors may be used to endorse or promote products
-//   derived from this software without specific prior written permission.
+//	* Redistributions of source code must retain the above copyright
+//	notice, this list of conditions and the following disclaimer.
+//	* Redistributions in binary form must reproduce the above copyright
+//	notice, this list of conditions and the following disclaimer in the
+//	documentation and/or other materials provided with the distribution.
+//	* Neither the name of the <organization> nor the
+//	names of its contributors may be used to endorse or promote products
+//	derived from this software without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -88,7 +88,7 @@ func New(zkServers []string) (*zkConn, error) {
 		myUtils.ExitWithNagiosCode(myGlobal.CRITICAL, err)
 	}
 	connection := &zkConn{
-		zkConnecion	:  connectedZK,
+		zkConnecion	: connectedZK,
 	}
 	return connection, nil
 }
